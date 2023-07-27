6fb1f5dfd95cf4a385d6333d79e0b1fd71f88040
Mr.Masterkey <839747705@qq.com>
2023-07-27 13:01:47 +0100

    running game in fullscreen mode

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         '''innitialize the game's settings.'''
         #Screen settings
-        self.screen_with = 1200
+        self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230,230,230)
         # Ship settings
