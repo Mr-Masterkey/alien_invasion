7fe2c86708cdbbe8d6f45d4e2c60d8d8bb3cd3c5
Mr.Masterkey <839747705@qq.com>
2023-08-12 15:06:47 +0100

    draw the button on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 class AlienInvasion:
     '''Overall class to manage game assets and behacior.'''
@@ -32,6 +33,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self._create_fleet()
+
+        #Make the play button
+        self.play_button = Button(self, 'Play')
 
     
 
@@ -111,6 +115,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)          
+
+        #Draw the play button if game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         #Make the most recently drawn screen visible.
         pygame.display.flip()
